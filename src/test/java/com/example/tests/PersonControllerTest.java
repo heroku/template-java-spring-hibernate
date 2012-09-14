82094ac0660c3b1c5058a8bd470b3571ab8f217b
Anand <anand@heroku.com>
2012-09-13 17:28:39 -0700

    Added a real test method.

--- src/test/java/com/example/tests/PersonControllerTest.java
+++ src/test/java/com/example/tests/PersonControllerTest.java
@@ -4,27 +4,42 @@ import junit.framework.TestCase;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.controller.PersonController;
+import com.example.model.Person;
+import com.example.service.PersonService;
 
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/applicationContext.xml" })
 
 public class PersonControllerTest extends TestCase {
-	@Test
+
+	@Autowired
+	PersonService personService;
+	
+ 	@Test
     public void testListPersons() throws Exception {
-		/*Map<String,Object> reqMap = new HashMap<String,Object>();
-		PersonController c= new PersonController();
+ 		Person p = new Person();
+		p.setFirstName("Test");
+		p.setLastName("Person");
+		personService.addPerson(p);
 		
-		String viewName = c.listPeople(reqMap);
-		*/
-		Assert.assertEquals(1, 1);
+		List<Person> persons = personService.listPeople();
+		Assert.assertEquals(1, persons.size());
    }
 
 }
