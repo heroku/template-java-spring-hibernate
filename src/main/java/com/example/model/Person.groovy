2fe643bd51205fb61aa53b2ff7d6a79b2e5d894a
Kunal Dabir <kunal.dabir@gmail.com>
2012-10-27 01:34:36 +0530

    Remove noise from the entity class

--- src/main/java/com/example/model/Person.groovy
+++ src/main/java/com/example/model/Person.groovy
@@ -9,35 +9,10 @@ public class Person {
 
     @Id
     @GeneratedValue
-    private Integer id;
+    Integer id
 
-    private String firstName;
+    String firstName
 
-    private String lastName;
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    String lastName
 
 }
